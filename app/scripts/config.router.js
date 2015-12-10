7a3c6f803991ce2a99be4e2bb12551fb608d540b
Liam Globit <guoylyy@gmail.com>
2015-12-11 01:26:30 +0800

    1. 日历的支持，首页日历添加

--- app/scripts/config.router.js
+++ app/scripts/config.router.js
@@ -65,7 +65,14 @@ angular.module('nevermore')
                 return $ocLazyLoad.load([
                   'scripts/controllers/portal/calendar.js',
                   'scripts/directives/portal/portal-footer.js',
-                  'scripts/directives/portal/portal-header.js'
+                  'scripts/directives/portal/portal-header.js',
+                  'lib/jquery/fullcalendar/fullcalendar.css',
+                  'lib/jquery/fullcalendar/theme.css',
+                  'lib/jquery/jquery-ui-1.10.3.custom.min.js',
+                  'lib/libs/moment.min.js',
+                  'lib/jquery/fullcalendar/fullcalendar.min.js',
+                  'ui.calendar',
+                  'scripts/controllers/app/app-calendar.js'
                 ]);
               }]
             }
