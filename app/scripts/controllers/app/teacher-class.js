fc6ad8bea1f931504147b21b54b81e522fb0314c
panyan <panyan@panyandeMacBook-Pro-2.local>
2015-12-12 00:43:44 +0800

    添加教师班级实验报告列表

--- app/scripts/controllers/app/teacher-class.js
+++ app/scripts/controllers/app/teacher-class.js
@@ -1,10 +1,11 @@
 'use strict';
 
-app.controller('TeacherClassCtrl', function($scope,$stateParams,qService,Exp,generalService,Clazz) {
+app.controller('TeacherClassCtrl', function($scope,$stateParams,qService,Exp,generalService,Clazz,StudentRecord) {
 
   $scope.course_name= $stateParams.name;
   $scope.course_number = $stateParams.number;
   var class_id = $stateParams.id;
+  var exp_id;
 
   var pageSize = generalService.pageSize();
 
@@ -17,15 +18,20 @@ app.controller('TeacherClassCtrl', function($scope,$stateParams,qService,Exp,gen
       totalItemNum:0
   };
 
+  $scope.records = {
+      data:[],
+      totalPageNum: 0,
+      curPageNum:1,
+      totalItemNum:0
+  };
+
   qService.tokenHttpGet(Exp.statusListByClazz, {
       "classId": class_id
   }).then(function(rc){
       $scope.exps = rc;
   });
 
-  $scope.view_student = function(pageNumber) {
-    $scope.pages = [];
-    $scope.tab="student";
+  $scope.page_student = function(pageNumber) {
     qService.tokenHttpGet(Clazz.studentListByPage, {
         "id": class_id,
         "pageSize": pageSize,
@@ -37,6 +43,27 @@ app.controller('TeacherClassCtrl', function($scope,$stateParams,qService,Exp,gen
 
   $scope.view_exp = function() {
     $scope.tab="experiment";
+  }
+
+  $scope.view_student = function(){
+    $scope.tab="student";
+  }
+
+  $scope.view_record = function(id){
+    $scope.tab = 'record';
+    exp_id = id;
+  }
+
+  $scope.page_record = function(pageNumber){
+
+    qService.tokenHttpGet(StudentRecord.recordListByPage,{
+        "clazzId": class_id,
+        "experimentId": exp_id,
+        "pageSize":pageSize,
+        "pageNumber":pageNumber
+    }).then(function(rc){
+        $scope.records = rc;
+    });
   }
 
 });
