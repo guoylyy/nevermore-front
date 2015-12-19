1616e484b40f5a2ad95ee7ad88e8edca8787cad3
panyan <panyan@panyandeMacBook-Pro-2.local>
2015-12-19 16:37:21 +0800

    完成相关问答存储

--- app/scripts/controllers/app/report.js
+++ app/scripts/controllers/app/report.js
@@ -4,6 +4,8 @@ app.controller('ReportCtrl',  function($scope, $http, $state, $localStorage) {
 
   $scope.report_step = 1;
 
+  $scope.completed_question = 0;
+
   if ($localStorage.report == null) {
     $http.get("tpl/app/report/test.json").success(function(data) {
       $scope.data = $localStorage.report = data;
@@ -19,5 +21,19 @@ app.controller('ReportCtrl',  function($scope, $http, $state, $localStorage) {
   $scope.last = function() {
     $scope.report_step--;
   }
+
+  $scope.$watch("data['7questions']",function(){
+    $scope.completed_question = 0;
+    $scope.data['7questions'].choice.forEach(function(data) {
+      if (data['choice']['answer']!=null&&data['choice']['answer']!=""&&data['choice']['answer']!=undefined) {
+        $scope.completed_question++;
+      }
+    });
+    $scope.data['7questions'].text.forEach(function(data) {
+      if (data['answer']['answer']!=null&&data['answer']['answer']!=""&&data['answer']['answer']!=undefined) {
+        $scope.completed_question++;
+      }
+    });
+  });
 
 });
