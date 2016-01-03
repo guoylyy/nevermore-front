9e345f187e91c218ea0e7c8bd0ca9aa27d5244fd
panyan <panyan@panyandeMacBook-Pro-2.local>
2016-01-03 11:37:36 +0800

    修复登陆重复进入逻辑

--- app/scripts/controllers/portal/portal.js
+++ app/scripts/controllers/portal/portal.js
@@ -2,15 +2,5 @@
 
 app.controller('PortalController', ['$scope','$rootScope','$state','$localStorage', function($scope, $rootScope, $state, $localStorage) {
 
-  if ($localStorage.token!=null) {
-    if ($localStorage.currentUser.show_role == 'STUDENT') {
-      $state.go('app.index.student-reservations',{title:'clazz'});
-    }else if ($localStorage.currentUser.show_role == 'TEACHER') {
-      $state.go('app.index.teacher-reservations',{title:'APPROVED'});
-    }else if ($localStorage.currentUser.show_role == 'ADMINISTRATOR') {
-      $state.go('app.account-admin.admin-account');
-    }
-
-  }
-
+  
 }]);
