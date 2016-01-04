04ffbc9a60c39726857596e138f156df13c4d700
Leooonard <398148357@qq.com>
2016-01-04 20:28:25 +0800

    修复portal header没绑currentUser

--- app/scripts/directives/portal/portal-header.js
+++ app/scripts/directives/portal/portal-header.js
@@ -12,7 +12,7 @@ angular.module('nevermore')
       templateUrl: 'tpl/portal/block/header.html',
       restrict: 'E',
       controller: function ($scope, sessionService, $rootScope, $localStorage, $state) {
-        var currentUser = sessionService.getCurrUser();
+        $scope.currentUser = sessionService.getCurrUser();
         $scope.showLoginButton = showLoginButton
       	
         $scope.head_click = function(){
