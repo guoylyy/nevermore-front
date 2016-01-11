04b11fd27dce2dc8445156c952c760d18dee8a70
Leooonard <398148357@qq.com>
2016-01-11 23:27:44 +0800

    添加课程选择页面

---
+++ app/scripts/controllers/app/student/class-selection.controller.js
@@ -0,0 +1,10 @@
+;void function(){
+	angular.module("nevermore")
+			.controller("StudentClassSelectionController", StudentClassSelectionController)
+
+	StudentClassSelectionController.$inject= ["$scope"]
+
+	function StudentClassSelectionController($scope){
+		
+	}
+}()
---
+++ app/scripts/controllers/app/teacher/class-selection.controller.js
@@ -0,0 +1,10 @@
+;void function(){
+	angular.module("nevermore")
+			.controller("TeacherClassSelectionController", TeacherClassSelectionController)
+
+	TeacherClassSelectionController.$inject= ["$scope"]
+
+	function TeacherClassSelectionController($scope){
+		
+	}
+}()
