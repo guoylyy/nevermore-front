04202588f9ddf1191cf8994286cfaf800bf52829
Mi Li <limisky@gmail.com>
2016-01-14 17:10:26 +0800

    1. 实现实验后台管理

---
+++ app/scripts/factories/refine/experiment-manage-factory.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * 实验相关 api
+ */
+angular.module('nevermore')
+  .factory('ExperimentManage', function($resource, sessionService, $rootScope) {
+    var baseUrl ="http://localhost:8080/manage/experiment";
+    return {
+      create: function(){
+        return $resource(baseUrl, {}, {
+          'post': {
+            method: 'POST',
+            headers: sessionService.headers()
+          }
+        });
+      },
+      experiment: function(){
+        return $resource(baseUrl + '/:id', {id:"@id"}, {
+          'get': {
+            method: 'GET',
+            headers: sessionService.headers()
+          },
+          'put': {
+            method: 'PUT',
+            headers: sessionService.headers()
+          },
+          'delete': {
+            method: 'DELETE',
+            headers: sessionService.headers()
+          }
+        });
+      },
+      page: function() {
+        return $resource(baseUrl + '/experiments?scope=list', {
+          pageSize:"@pageSize",
+          pageNum:"@pageNum"
+        }, {
+          'get': {
+            method: 'GET',
+            headers: sessionService.headers()
+          }
+        });
+      },
+      all: function(){
+        return $resource(baseUrl + '/experiments?scope=all', {
+        }, {
+          'get': {
+            method: 'GET',
+            headers: sessionService.headers()
+          }
+        });
+      }
+    };
+  });
