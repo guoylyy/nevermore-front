b8f845408230c0ae527abc3466a9236d412b4fdf
alfred <alfred_yuan@192.168.3.18>
2016-01-17 00:03:59 +0800

    1. 完成个人信息修改，职务暂时去除，电子邮件没有该字段 2. 完成密码更新页面 3. 头像上传需要后续支持，现在文件已经可以上传，但不能绑定

--- app/scripts/controllers/app/profile-password.js
+++ app/scripts/controllers/app/profile-password.js
@@ -4,24 +4,32 @@ app.controller('ProfilePasswordCtrl', ['$scope', '$rootScope','Account', 'Toaste
 function($scope, $rootScope, Account, ToasterTool, sessionService) {
   $scope.password = {
     passwordEdit: {
-      newPassword: null,
-      oldPassword: null
+      oldPassword: null,
+      password: null,
+      confirmPassword: null
     }
   };
 
   var currentUser = sessionService.getCurrentUser();
 
   $scope.updatePassword = function() {
-    Account.password().put({'id': currentUser.id}, $scope.password.passwordEdit,
-      function success(data) {
-        ToasterTool.success("更新密码成功");
+    angular.forEach($scope.password.passwordEdit, function(value, key, obj) {
+      obj[key] = md5(value);
+    })
 
+    Account.password().put($scope.password.passwordEdit).$promise.
+    then(
+      function success(data) {
+        if (data.success) {
+          ToasterTool.success("更新密码成功");
+        } else {
+          ToasterTool.error("更新密码失败");
+        }
         $scope.password.passwordEdit = {};
         $scope.passwordForm.$setPristine();
       },
-      function error(data) {
-        ToasterTool.error("更新密码失败");
-      }
-    );
-  }
+      function error(error) {
+        console.log(error);
+      })
+    }
 }]);
