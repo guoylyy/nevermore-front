93b0cf51cf3d4eb40d0859ae7107c90d192659b2
Liam Globit <guoylyy@gmail.com>
2016-01-17 17:06:14 +0800

    1. 完成添加学生 2. 完成班级移除学生 3. 完成学生列表 （搜索和上传学生excel文件没有完成）

--- app/scripts/factories/refine/clazz.factory.js
+++ app/scripts/factories/refine/clazz.factory.js
@@ -3,15 +3,18 @@
   'use strict';
 
   angular.module('nevermore')
-    .factory('clazzFactory', function($resource, sessionService, $rootScope) {
+    .factory('ClazzFactory', function($resource, sessionService, $rootScope) {
     var apiUrl = base_Url+'/clazz'
 
     return {
       teacherClazzList: teacherClazzList,
       mainPage: mainPage,
       files: files,
+      file: file,
+      students: students,
+      student: student
     }
-
+    //教师班级列表
     function teacherClazzList(){
       return $resource(apiUrl + "/teacherClazzList", null, {
         get: {
@@ -20,7 +23,7 @@
         }
       })
     }
-
+    //课程主页
     function mainPage(){
       return $resource(apiUrl + "/:id/mainPage", {
         id: "@classID"
@@ -31,7 +34,18 @@
         }
       })
     }
-
+    //上传文件
+    function file(){
+      return $resource(apiUrl + "/:id/file", {
+        id: "@classID",
+      }, {
+        post: {
+          method: "POST",
+          headers: sessionService.headers(),
+        }
+      })
+    }
+    //获取文件
     function files(){
       return $resource(apiUrl + "/:id/files", {
         id: "@classID",
@@ -42,9 +56,37 @@
         }
       })
     }
+    //学生列表管理
+    function students(){
+      return $resource(apiUrl + "/:id/students", {
+        id: "@id",
+      }, {
+        get: {
+          method: "GET",
+          headers: sessionService.headers(),
+        },
+        post: {
+          method: "POST",
+          headers: sessionService.headers(),
+        },
+        delete: {
+          method: "DELETE",
+          headers: sessionService.headers(),
+        }
+      })
+    }
+    //添加一个学生
+    function student(){
+      return $resource(apiUrl + "/:id/student", {
+        id: "@id",
+      }, {
+        post: {
+          method: "POST",
+          headers: sessionService.headers(),
+        }
+      })
+    }
 
   })
 
 }()
-
-
