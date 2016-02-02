d06f49e5312691ed90acda652da6aff60f6fce4a
Leooonard <398148357@qq.com>
2016-02-02 21:56:06 +0800

    增加虚拟实验部分功能和过期预约查看功能

--- app/scripts/controllers/portal/Index.controller.js
+++
@@ -1,8 +0,0 @@
-;void function(){
-
-  app.controller("IndexController", IndexController)
-  IndexController.$inject = ['$scope', '$localStorage','sessionService','$state']
-
-  function IndexController($scope, $localStorage, sessionService, $state) {
-  }
-}()
--- app/scripts/filters/Experiment.filter.js
+++ app/scripts/filters/Experiment.filter.js
@@ -1,14 +1,26 @@
-'use strict';
+;
+void
+function() {
+    "use strict";
 
-
-angular.module('nevermore')
-  .filter('activeFilter', function () {
-  	return function(active){
-			if(active){
-		      return "开放"
-		    }else{
-		      return "关闭"
-		    }
-  	}
-
-  })
+    angular.module("nevermore")
+        .filter("activeFilter", function() {
+            return function(active) {
+                if (active) {
+                    return "开放";
+                } else {
+                    return "关闭";
+                }
+            };
+        })
+    angular.module("nevermore")
+        .filter("nmAppointmentStatus", function() {
+            return function(status) {
+                if (status.code === "APPLY") {
+                    return "已失效";
+                } else {
+                    return "已完成";
+                }
+            };
+        });
+}();
